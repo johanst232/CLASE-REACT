5cb588761b4fd54d83edd4fb24b8ee7a29864451
joe <johanst232@gmail.com>
2024-09-08 15:47:02 -0300

    proyecto

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>mi app</h1>;
+}
+
+export default App;
---
+++ src/Navbar.jsx
@@ -0,0 +1,7 @@
+export const navbar = () => {
+  return (
+    <div>
+      <h1>navegador</h1>
+    </div>
+  );
+};
